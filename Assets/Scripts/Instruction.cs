f660cc2d226f2504accb6ba729978c8e3206b98d
Lucas Soares <lucassoares1793@gmail.com>
2017-12-07 19:16:25 -0200

    Processor simulation working

--- Assets/Scripts/Instruction.cs
+++ Assets/Scripts/Instruction.cs
@@ -1,32 +1,93 @@
-﻿// using System.Collections;
-// using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public enum InstructionName{
-// 	NOOP
-// 	// Listar todas as instruções
-// }
+public class Instruction {
+	
+	// Instruction code separator
+	public static readonly char[] SEPARATOR = {' '};
+	public static readonly string NOOP = "NOOP - - -";
+	// MIPS Instruction Set for this simulation
+	public static readonly Dictionary<string, char> InstructionSet = 
+		new Dictionary<string, char>{
+			{ "NOOP", 	'N' },
+			
+			{ "ADD",	'R' },
+			{ "SUB",	'R' },
+			{ "MULT",	'R' },
+			{ "DIV",	'R' },
+			{ "AND",	'R' },
+			{ "OR",		'R' },
+			{ "XOR", 	'R' },
+			{ "SLL", 	'R' },
+			{ "SRL", 	'R' },
+			
+			{ "LW", 	'I' },
+			{ "SW", 	'I' },
+			{ "BEZ", 	'I' },
+			{ "BNZ", 	'I' },
+			{ "ADDI", 	'I' },
+			{ "SUBI", 	'I' },
+			{ "MULTI", 	'I' },
+			{ "DIVI", 	'I' },
+			{ "ANDI", 	'I' },
+			{ "ORI", 	'I' },
+			{ "XORI", 	'I' },
+			{ "SLLI", 	'I' },
+			{ "SRLI", 	'I' },
 
-// public enum States {
-// 	Fetch,
-// 	Decode,
-// 	Execute,
-// 	Memory,
-// 	Writeback
-// }
+			{ "JMP", 	'J' }
+	    };
 
-// public class Instruction {
+	// Object attributes
+	public string icode { get; private set; }	// Instruction in format "add r0 r1 r2"
 
-// 	public InstructionName name;
-// 	public bool[] states;
+	public string iname	{ get; private set; }	// Instruction name as in "add"
+	public char type 	{ get; private set; }
+	public int op1		{ get; private set; }	// Instruction first operand "r0"
+	public int op2		{ get; private set; }	// Instruction second operand "r1"
+	public int op3		{ get; private set; }	// Instruction third operand "r2"
 
-// 	public Instruction(){
-// 		this.states = new bool[Enum.GetNames(typeof(States)).Length];
-// 	}
 
-// 	public Instruction(bool[] states){
-// 		if(states.Length != Enum.GetNames(typeof(States)).Length)
-// 			throw InvalidArgumentException();
+	// 0-argument constructor for array creation
+	public Instruction(){
+		this.icode = NOOP;
+		this.Parse(NOOP);
+		this.type = Instruction.InstructionSet[this.iname];
+	}
 
-// 		this.states = states;
-// 	}
-// }
+	public Instruction(string icode){
+		icode = icode.ToUpper();
+		this.icode = icode;
+		this.Parse(icode);
+		this.type = Instruction.InstructionSet[this.iname];
+	}
+
+	private void Parse(string icode){
+		
+		string[] parts = icode.Split(SEPARATOR);
+		this.iname = parts[0];
+
+		// if(parts[1][0] == '-') // Value doesnt exist in instruction
+		// 	this.op1 = null;
+		if(parts[1][0] == 'R') // Differentiate register from immediate value
+			this.op1 = parts[1][1] - '0';
+		else if(parts[1][0] != '-') // Immediate value
+			this.op1 = Int32.Parse(parts[1]);
+
+		// if(parts[2][0] == '-') // Value doesnt exist in instruction
+		// 	this.op2 = null;
+		if(parts[2][0] == 'R') // Differentiate register from immediate value
+			this.op2 = parts[2][1] - '0';
+		else if(parts[2][0] != '-') // Immediate value
+			this.op2 = Int32.Parse(parts[2]);
+
+		// if(parts[3][0] == '-') // Value doesnt exist in instruction
+		// 	this.op3 = null;
+		if(parts[3][0] == 'R') // Differentiate register from immediate value
+			this.op3 = parts[3][1] - '0';
+		else if(parts[3][0] != '-') // Immediate value
+			this.op3 = Int32.Parse(parts[3]);
+	}
+}
--- Assets/Scripts/Processor.cs
+++ Assets/Scripts/Processor.cs
@@ -1,19 +1,212 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
-public class Processor {
+// public class NamedArrayAttribute : PropertyAttribute {
+//     public readonly string[] names;
+//     public NamedArrayAttribute(string[] names) { this.names = names; }
+// }
 
+[UnityEngine.SerializeField]
+public class Processor : MonoBehaviour {
+
+	/* Serializable variables so we can edit and see them in Unity Editor */
+	public string[] code = new string[1024];
+	
+	// [NamedArrayAttribute (new string[] {"Fetch", "Decode", "Execute", "Memory", "Writeback"})]
+	public string[] pipestr;
+
+	public List<Instruction> instructionMemory; // Memory is word oriented
+	public int[] dataMemory = new int[1024]; 	// Memory is word oriented
 	public int[] registerBank = new int[4];
-	public List<Instruction> instructionMemory;
 
-	private FetchUnit fu;
-    private IFId ifId;
-    private Decode decode;
-    private IdEx idEx;
-    private Execute execute;
-    private ExMem exMem;
-    private Memory memory;
-    private MemWb memWb;
-    private WriteBack writeBack;
+	// Instruction array constructor intialize to NOOP
+	public Instruction[] pipeline = new Instruction[5];
+
+	public int pc = 0;
+	public int branchPc = 0;
+	public int regA = 0, regB = 0;
+	public int regO0 = 0;
+	public int regO1 = 0;
+	public int writeToMem = 0;
+
+
+
+	void Start(){
+		instructionMemory = new List<Instruction>();
+		for(int i = 0; i < code.Length; i++)
+			instructionMemory.Add(new Instruction(code[i]));
+
+		for(int i = 0; i < 5; i++){
+			pipeline[i] = new Instruction();
+			pipestr[i] = pipeline[i].icode;
+		}
+	}
+
+	void Update(){
+
+		if(Input.GetKeyDown("h")){  }
+
+		if(Input.GetKeyDown("n")){
+
+			Instruction instr = instructionMemory[pc];
+
+			for(int i = 4; i > 0; i--){ // Move pipeline forward
+				pipeline[i] = pipeline[i-1];
+				pipestr[i] = pipeline[i].icode;
+			}
+			pipestr[0] = pipeline[0].icode;
+
+			InstructionWriteback(pipeline[4], regO1);
+			InstructionMemory(pipeline[3], regO0, writeToMem, ref regO1);
+			InstructionExec(pipeline[2], regA, regB, ref regO0);
+			InstructionDecode(pipeline[1], ref regA, ref regB);
+			InstructionFetch(pc, ref pipeline[0]);
+			
+			// Avoid Writeback directly writing to register bank and screwing up
+			// Fetch register bank
+			// registerBank = (int[]) tmpBank.Clone(); 
+		}
+	}
+
+	/* Pipeline units */
+    public void InstructionFetch(int address, 
+    	ref Instruction instr){
+
+    	instr = instructionMemory[this.pc++];
+    	pipestr[0] = instr.icode;
+    }
+
+    public void InstructionDecode(Instruction instr, 
+    	ref int outputA, ref int outputB){
+
+    	switch(instr.type){
+		// R
+		case 'R':
+			// op1 is the destiny register
+			outputA = this.registerBank[instr.op2]; // First operand
+			outputB = this.registerBank[instr.op3]; // Second operand
+			break;
+
+		// Index
+		case 'I':
+			// op1 is the destiny register or is not used
+			outputA = this.registerBank[instr.op2]; // Get value from register
+			outputB = instr.op3;					// Immediate
+			break;
+		
+		// JUMP
+		case 'J':
+			outputA = instr.op3; // Immediate
+			break;
+		
+		// NOOP
+		case 'N':
+			break;
+    	}
+    }
+
+    public void InstructionExec(Instruction instr, int inputA, int inputB, 
+    	ref int output){
+
+    	switch(instr.iname){
+
+    	// NOOP
+		case "NOOP":
+			break;
+			
+		case "SW":
+			this.writeToMem = registerBank[instr.op1]; // This works?
+			output = inputA + inputB;
+			break;
+
+		case "LW":
+		case "ADD":
+		case "ADDI":
+			output = inputA + inputB;
+			break;
+
+		case "SUB":
+		case "SUBI":
+			output = inputA - inputB;
+			break;
+
+		case "MULT":
+		case "MULTI":
+			output = inputA * inputB;
+			break;
+
+		case "DIV":
+		case "DIVI":
+			output = inputA / inputB;
+			break;
+
+		case "AND":
+		case "ANDI":
+			output = inputA & inputB;
+			break;
+
+		case "OR":
+		case "ORI":
+			output = inputA | inputB;
+			break;
+
+		case "XOR":
+		case "XORI":
+			output = inputA ^ inputB;
+			break;
+
+		case "SLL":
+		case "SLLI":
+			output = inputA << inputB;
+			break;
+
+		case "SRL":
+		case "SRLI":
+			output = inputA >> inputB;
+			break;
+
+		case "BEZ":
+			output = (inputA == 0) ? inputB : pc;
+			break;
+
+		case "BNZ":
+			output = (inputA != 0) ? inputB : pc;
+			break;
+		
+		case "JMP":
+			output = inputA;
+			break;
+		}
+    }
+
+    public void InstructionMemory(Instruction instr, int address, int memWriteD, 
+    	ref int memReadD){
+
+		// RegO1 = RegO0 (reaproveitando os mesmos registradores)
+		memReadD = address;
+
+		if(instr.iname.Equals("LW"))
+			memReadD = dataMemory[address];
+		else if(instr.iname.Equals("SW"))
+			dataMemory[address] = memWriteD;
+    }
+
+    public void InstructionWriteback(Instruction instr, int input){
+    	// Dont write to register bank if instruction doesnt have WB stage
+    	// Instruction that dont write to register bank:
+    	// NOOP JMP BRANCH0 BRANCH!0 STORE
+    	if( instr.type != 'J' && 
+    		instr.type != 'N' && 
+    	   !instr.iname.Equals("BEZ") && 
+    	   !instr.iname.Equals("BNZ") && 
+    	   !instr.iname.Equals("SW"))
+    			
+    		registerBank[instr.op1] = input;
+    }
+
+	/* End pipeline units */
+
+
 
 }
