8c731008c8987e252bc70ad701a42966e3f9d7e1
Rafael Monteiro <rafaelmonteiro95@gmail.com>
2017-12-08 07:58:52 +0000

    Created ULA and register file prefabs

---
+++ Assets/Scripts/RegisterFileController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[UnityEngine.SerializeField]
+public class RegisterFileController : MonoBehaviour {
+
+    public Text r0;
+    public Text r1;
+    public Text r2;
+    public Text r3;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void setName(string newName)
+    {
+        nameText.text = newName;
+    }
+
+    public void setValues(int[] newValues)
+    {
+       r0.text = newValues[0].toString();
+       r1.text = newValues[1].toString();
+       r2.text = newValues[2].toString();
+       r3.text = newValues[3].toString();
+    }
+}
---
+++ Assets/Scripts/UlaController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[UnityEngine.SerializeField]
+public class UlaController : MonoBehaviour {
+
+    public Text nameText;
+    public Text inputAValue;
+    public Text inputBValue;
+    public Text outputValue;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void setName(string newName)
+    {
+        nameText.text = newName;
+    }
+
+    public void setInputA(int newValue)
+    {
+        this.inputAValue.text = newValue.ToString();
+    }
+
+    public void setInputB(int newValue)
+    {
+        this.inputBValue.text = newValue.ToString();
+    }
+
+    public void setOutput(int newValue)
+    {
+        this.outputValue.text = newValue.ToString();
+    }
+
+}
