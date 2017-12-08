8182ed4efa299b1057ab07770a4dcac152806887
Rafael Monteiro <rafaelmonteiro95@gmail.com>
2017-12-08 10:21:12 +0000

    mandando lixo pro iron

---
+++ Assets/Scripts/MemoryPanelController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[UnityEngine.SerializeField]
+public class MemoryPanelController : MonoBehaviour {
+
+    public GameObject prefab;
+    public Transform content;
+
+    ListEntryController[] entries;
+    
+	// Use this for initialization
+	void Start () {
+
+        for(int i = 0; i < 25; i++)
+        {
+            Instantiate(prefab,content);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+	}
+}
--- Assets/Scripts/RegisterFileController.cs
+++ Assets/Scripts/RegisterFileController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 [UnityEngine.SerializeField]
@@ -20,16 +21,11 @@ public class RegisterFileController : MonoBehaviour {
 		
 	}
 
-    public void setName(string newName)
-    {
-        nameText.text = newName;
-    }
-
     public void setValues(int[] newValues)
     {
-       r0.text = newValues[0].toString();
-       r1.text = newValues[1].toString();
-       r2.text = newValues[2].toString();
-       r3.text = newValues[3].toString();
+       r0.text = newValues[0].ToString();
+       r1.text = newValues[1].ToString();
+       r2.text = newValues[2].ToString();
+       r3.text = newValues[3].ToString();
     }
 }
